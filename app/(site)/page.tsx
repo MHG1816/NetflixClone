01bfb38fb358f66623336a626b7a4f03213f5318
MHG1816 <hammaguibeneess@gmail.com>
2023-09-25 13:00:16 +0200

    getCurrentUser

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+"use client";
 
+import { signOut } from "next-auth/react"
 export default function Home() {
   return (
     <>
       <h1 className="text-red-500"> sdmkws </h1>
+      <button
+        onClick={() => signOut()} 
+        className="h-10 w-full bg-white"
+      >
+        Sign out
+      </button>
     </>
   )
 }
