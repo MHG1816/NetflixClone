a1162fbda178a22c73a8c73273edb2dc4a08a3f5
MHG1816 <hammaguibeneess@gmail.com>
2023-09-26 21:32:28 +0200

    A custom hook

---
+++ app/hooks/useFavorites.ts
@@ -0,0 +1,19 @@
+import useSWR from "swr";
+import fetcher from "../lib/fetcher";
+
+/**
+ * A custom hook that fetches data 
+ * from the "/api/favorites" endpoint 
+ * using the useSWR hook. 
+ * It returns the fetched data, 
+ * any error that occurred during the fetch, 
+ * a loading state, 
+ * and a function to mutate the data.
+ */
+const useFavorites = () => useSWR("/api/favorites", fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+});
+
+export default useFavorites;
