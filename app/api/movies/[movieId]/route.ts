bc975a2e34eb396a8091bc4afa2d06324ec8d4aa
MHG1816 <hammaguibeneess@gmail.com>
2023-09-27 17:48:20 +0200

    single Movie Endpoint

---
+++ app/api/movies/[movieId]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/lib/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface IParams{
+    movieId : string
+}
+export default async function GET({ params } : { params : IParams }) {
+    try {
+        const currentUser = await getCurrentUser();
+
+        if(!currentUser?.email) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const { movieId } = params;
+
+        if(!movieId || typeof movieId !== 'string'){
+            return new NextResponse("Invalid ID", { status : 400 });
+        };
+
+        const movie = await prisma.movie.findUnique({
+            where:{
+                id: movieId
+            }
+        });
+
+        if (!movie){
+            return new NextResponse("No movie is found", { status : 400});
+        };
+
+        return NextResponse.json(movie);
+    }catch(error){
+        console.log(error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+    
+}
